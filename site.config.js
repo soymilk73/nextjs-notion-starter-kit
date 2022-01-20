1505f57e2782e49915fd5c8be012164cc7fd2a49
soymilk73 <98071702+soymilk73@users.noreply.github.com>
2022-01-20 11:30:47 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,15 +1,15 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'd62b7a260636456b92334b25a5c1f456',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: '极地找饭豆奶',
+  domain: 'https://stealth-airship-8ee.notion.site/d62b7a260636456b92334b25a5c1f456',
+  author: '极地找饭豆奶',
 
   // open graph metadata (optional)
   description: 'Example site description',
